533605f8551b3010703dd2756a92f58e5c648dff
Nathan Hardy <nhardy@live.com.au>
2015-01-19 10:17:56 +1100

    Update database schema
    * Removed points field on game_users - this can now be calculating the
    number of times a uid appears as winner_id on game_czar
    * game_moves will use a timestamp (time) to order cards for a multiplace
    black card

--- pycah/db/create_database.sql
+++ pycah/db/create_database.sql
@@ -5,17 +5,18 @@ DROP TABLE IF EXISTS white_cards;
 DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS game_users;
 DROP TABLE IF EXISTS game_expansions;
+DROP TABLE IF EXISTS game_czar;
 DROP TABLE IF EXISTS game_moves;
 
 CREATE TABLE users (
-	uid BIGSERIAL PRIMARY KEY NOT NULL,
+	uid BIGSERIAL PRIMARY KEY,
 	username CHARACTER VARYING(255) NOT NULL UNIQUE,
 	password CHARACTER VARYING(255) NOT NULL,
 	salt CHARACTER VARYING(255) NOT NULL
 );
 
 CREATE TABLE expansions (
-	eid SMALLSERIAL PRIMARY KEY NOT NULL,
+	eid SMALLSERIAL PRIMARY KEY,
 	name CHARACTER VARYING(255) NOT NULL UNIQUE,
 	description TEXT
 );
@@ -46,20 +47,39 @@ CREATE TABLE games (
 CREATE TABLE game_users (
 	gid BIGINT NOT NULL,
 	uid BIGINT NOT NULL,
-	points SMALLINT NOT NULL,
+	PRIMARY KEY (gid, uid),
 	FOREIGN KEY (gid) REFERENCES games(gid),
-	FOREIGN KEY (uid) REFERENCES users(uid),
-	UNIQUE (gid, uid)
+	FOREIGN KEY (uid) REFERENCES users(uid)
 );
 
 CREATE TABLE game_expansions (
 	gid BIGINT NOT NULL,
 	eid SMALLINT NOT NULL,
+	PRIMARY KEY (gid, eid),
 	FOREIGN KEY (gid) REFERENCES games(gid),
 	FOREIGN KEY (eid) REFERENCES expansions(eid)
 );
 
---CREATE TABLE game_moves (
---	gid BIGINT NOT NULL,
---	uid BIGINT NOT NULL,
---);
+CREATE TABLE game_czar (
+	gid BIGINT NOT NULL,
+	round SMALLSERIAL,
+	czar_id BIGINT NOT NULL,
+	winner_id BIGINT,
+	PRIMARY KEY (gid, round),
+	FOREIGN KEY (gid) REFERENCES games(gid),
+	FOREIGN KEY (czar_id) REFERENCES users(uid),
+	FOREIGN KEY (winner_id) REFERENCES users(uid)
+);
+
+CREATE TABLE game_moves (
+	gid BIGINT NOT NULL,
+	round SMALLINT NOT NULL,
+	uid BIGINT NOT NULL,
+	cid SMALLINT NOT NULL,
+	time TIMESTAMP NOT NULL, -- For ordering of white card placements
+	PRIMARY KEY (gid, round, uid, time),
+	FOREIGN KEY (gid) REFERENCES games(gid),
+	FOREIGN KEY (round) REFERENCES game_czar(round)
+	FOREIGN KEY (uid) REFERENCES users(uid),
+	FOREIGN KEY (cid) REFERENCES white_cards(cid)
+);
