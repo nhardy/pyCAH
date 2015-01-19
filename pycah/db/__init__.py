3a05619c1e8e65ea57199edab541462d8f078443
Nathan Hardy <nhardy@live.com.au>
2015-01-19 16:44:38 +1100

    Moved schema intialisation to __init__.py

--- pycah/db/__init__.py
+++ pycah/db/__init__.py
@@ -1,12 +1,19 @@
 import psycopg2
 
+c = psycopg2.connect(user='postgres', password='password')
+c.set_session(autocommit=True)
+cur = c.cursor()
 try:
-  c = psycopg2.connect(user='postgres', password='password')
-  c.set_session(autocommit=True)
-  c.cursor().execute('CREATE DATABASE pycah;')
-except psycopg2.ProgrammingError:
-  pass
-finally:
+  cur.execute('CREATE DATABASE pycah;')
+  c.commit()
   c.close()
+  c = psycopg2.connect(database='pycah', user='postgres', password='password')
+  c.set_session(autocommit=True)
+  cur = c.cursor()
+  cur.execute(open('./pycah/db/create_database.sql').read())
+  c.commit()
+  c.close() 
+except psycopg2.ProgrammingError:
+  c.close() 
 
 connection = psycopg2.connect(database='pycah', user='postgres', password='password')
--- pycah/db/test.py
+++
@@ -1,7 +0,0 @@
-from . import connection
-
-connection.set_session(autocommit=True)
-cursor = connection.cursor()
-cursor.execute(open('create_database.sql').read())
-cursor.close()
-connection.close()
