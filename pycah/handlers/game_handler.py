53d1433ab0dc51930ce6928311d459fd59e7abd8
Nathan Hardy <nhardy@live.com.au>
2015-01-20 15:03:33 +1100

    Updated home/game pages

--- pycah/handlers/game_handler.py
+++ pycah/handlers/game_handler.py
@@ -2,4 +2,4 @@ import tornado.web
 
 class GameHandler(tornado.web.RequestHandler):
   def get(self):
-    self.render('game.html', handler=self)
+    self.render('game.html', handler=self, title='Game')
--- pycah/handlers/home_handler.py
+++ pycah/handlers/home_handler.py
@@ -2,4 +2,4 @@ import tornado.web
 
 class HomeHandler(tornado.web.RequestHandler):
   def get(self):
-    self.render('home.html', handler=self)
+    self.render('home.html', handler=self, title='Home')
