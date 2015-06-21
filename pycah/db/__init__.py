9c786c82671ade46e7af309fd597d5eac93a75b0
Nathan Hardy <nhardy@live.com.au>
2015-06-21 16:00:15 +1000

    Fix database connectivity on Linux.

--- pycah/db/__init__.py
+++ pycah/db/__init__.py
@@ -1,13 +1,13 @@
 import psycopg2
 
-c = psycopg2.connect(user='postgres', password='password')
+c = psycopg2.connect(user='postgres', password='password', host='127.0.0.1')
 c.set_session(autocommit=True)
 cur = c.cursor()
 try:
   cur.execute('CREATE DATABASE pycah;')
   c.commit()
   c.close()
-  c = psycopg2.connect(database='pycah', user='postgres', password='password')
+  c = psycopg2.connect(database='pycah', user='postgres', password='password', host='127.0.0.1')
   c.set_session(autocommit=True)
   cur = c.cursor()
   cur.execute(open('./pycah/db/create_database.sql').read())
@@ -16,4 +16,4 @@ try:
 except psycopg2.ProgrammingError:
   c.close() 
 
-connection = psycopg2.connect(database='pycah', user='postgres', password='password')
+connection = psycopg2.connect(database='pycah', user='postgres', password='password', host='127.0.0.1')
